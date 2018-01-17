16142e374ca5fcecc26fe471263a5a614697e265
Schtekt <totimom@live.se>
2018-01-17 15:58:58 +0100

    Added function to create a triangle and linked opengl32.lib

--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
 #include <Windows.h>
+
 #include <GL\glew.h>
 #include <GLFW\glfw3.h>
-
+#include <GL\GL.h>
 #include <iostream>
+
+GLuint VertexArrayID;
+GLuint Vertexbuffer;
+
+void createTriangle()
+{
+	static const GLfloat gVertexBufferData[] = {
+		-1.0f, -1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f,
+		0.0f,  1.0f, 0.0f,
+	};
+
+
+	// Generate 1 buffer, put the resulting identifier in vertexbuffer
+	glGenBuffers(1, &Vertexbuffer);
+	// The following commands will talk about our 'vertexbuffer' buffer
+	glBindBuffer(GL_ARRAY_BUFFER, Vertexbuffer);
+	// Give our vertices to OpenGL.
+	glBufferData(GL_ARRAY_BUFFER, sizeof(gVertexBufferData), gVertexBufferData, GL_STATIC_DRAW);
+}
 
 int main() {
 
@@ -11,6 +32,7 @@ int main() {
 		fprintf(stderr, "Failed to initialize GLFW\n");
 		return -1;
 	}
+
 
 	glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // We want OpenGL 3.3
@@ -21,6 +43,7 @@ int main() {
 																   // Open a window and create its OpenGL context
 	GLFWwindow* window; // (In the accompanying source code, this variable is global for simplicity)
 	window = glfwCreateWindow(1024, 768, "3DProj", NULL, NULL);
+
 	if (window == NULL) {
 		fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
 		glfwTerminate();
@@ -35,13 +58,35 @@ int main() {
 		fprintf(stderr, "Failed to initialize glew\n");
 		return -1;
 	}
+
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, 1);
+
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+
+	createTriangle();
+
 	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0)
 	{
 
 		glfwPollEvents();
-
+		// 1rst attribute buffer : vertices
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, Vertexbuffer);
+		glVertexAttribPointer(
+			0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			0,                  // stride
+			(void*)0            // array buffer offset
+		);
+		// Draw the triangle !
+		glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+		glDisableVertexAttribArray(0);
 		glfwSwapBuffers(window);
 	}
+
+
 	return 0;
 }
