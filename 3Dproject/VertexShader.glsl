6bb7000d632401ddad139173f03eff37aa1c2434
Schtekt <totimom@live.se>
2018-01-22 14:51:05 +0100

    Added colours to the cube, enabled depth buffer and included glm/transform

--- 3Dproject/VertexShader.glsl
+++ 3Dproject/VertexShader.glsl
@@ -1,7 +1,12 @@
 #version 430
 layout(location = 0) in vec3 vertexPositionModelSpace;
+layout(location = 1) in vec3 vertexColor;
 uniform mat4 MVP;
+
+out vec3 fragmentColor;
+
 void main()
 {
 gl_Position = MVP*vec4(vertexPositionModelSpace, 1.0);
+fragmentColor = vertexColor;
 }
--- 3Dproject/main.cpp
+++ 3Dproject/main.cpp
@@ -1,9 +1,11 @@
 #include <GL\glew.h>
 #include <GLFW\glfw3.h>
 
+#include <GLM\mat4x4.hpp>
 #include <GLM\glm.hpp>
-//#include <GLM\gtx\transform.hpp>
 #include <GLM\gtc\matrix_transform.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <GLM\gtx\transform.hpp>
 
 #include <windows.h>
 #include <fstream>
@@ -11,12 +13,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 int width = 1024;
 int height = 768;
 
 GLFWwindow* Window;
 GLuint VertexArrayID;
 GLuint Vertexbuffer;
+GLuint colorbuffer;
 GLuint gShaderProgram;
 GLuint MatrixID;
 
@@ -91,6 +96,49 @@ void createCube()
 	glBindBuffer(GL_ARRAY_BUFFER, Vertexbuffer);
 	// Give our vertices to OpenGL.
 	glBufferData(GL_ARRAY_BUFFER, sizeof(gVertexBufferData), gVertexBufferData, GL_STATIC_DRAW);
+
+	// One color for each vertex. They were generated randomly.
+	static const GLfloat g_color_buffer_data[] = {
+		0.583f,  0.771f,  0.014f,
+		0.609f,  0.115f,  0.436f,
+		0.327f,  0.483f,  0.844f,
+		0.822f,  0.569f,  0.201f,
+		0.435f,  0.602f,  0.223f,
+		0.310f,  0.747f,  0.185f,
+		0.597f,  0.770f,  0.761f,
+		0.559f,  0.436f,  0.730f,
+		0.359f,  0.583f,  0.152f,
+		0.483f,  0.596f,  0.789f,
+		0.559f,  0.861f,  0.639f,
+		0.195f,  0.548f,  0.859f,
+		0.014f,  0.184f,  0.576f,
+		0.771f,  0.328f,  0.970f,
+		0.406f,  0.615f,  0.116f,
+		0.676f,  0.977f,  0.133f,
+		0.971f,  0.572f,  0.833f,
+		0.140f,  0.616f,  0.489f,
+		0.997f,  0.513f,  0.064f,
+		0.945f,  0.719f,  0.592f,
+		0.543f,  0.021f,  0.978f,
+		0.279f,  0.317f,  0.505f,
+		0.167f,  0.620f,  0.077f,
+		0.347f,  0.857f,  0.137f,
+		0.055f,  0.953f,  0.042f,
+		0.714f,  0.505f,  0.345f,
+		0.783f,  0.290f,  0.734f,
+		0.722f,  0.645f,  0.174f,
+		0.302f,  0.455f,  0.848f,
+		0.225f,  0.587f,  0.040f,
+		0.517f,  0.713f,  0.338f,
+		0.053f,  0.959f,  0.120f,
+		0.393f,  0.621f,  0.362f,
+		0.673f,  0.211f,  0.457f,
+		0.820f,  0.883f,  0.371f,
+		0.982f,  0.099f,  0.879f
+	};
+	glGenBuffers(1, &colorbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
 }
 
 void render()
@@ -110,7 +158,20 @@ void render()
 		0,                  // stride
 		(void*)0            // array buffer offset
 	);
-	// Draw the triangle !
+
+	// 2nd attribute buffer : colors
+	glEnableVertexAttribArray(1);
+	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+	glVertexAttribPointer(
+		1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+		3,                                // size
+		GL_FLOAT,                         // type
+		GL_FALSE,                         // normalized?
+		0,                                // stride
+		(void*)0                          // array buffer offset
+	);
+
+	// Draw the Cube!
 	glDrawArrays(GL_TRIANGLES, 0, 3*12); // Starting from vertex 0; 3 vertices total -> 1 triangle
 	glDisableVertexAttribArray(0);
 }
@@ -243,10 +304,15 @@ int main()
 	//createTriangle();
 	createCube();
 	glm::mat4 mvp = makeMatrices();
+	// Enable depth test
+	glEnable(GL_DEPTH_TEST);
+	// Accept fragment if it closer to the camera than the former one
+	glDepthFunc(GL_LESS);
 	do {
 		// Draw nothing, see you in tutorial 2 !
 		// Send our transformation to the currently bound shader, in the "MVP" uniform
 		// This is done in the main loop since each model will have a different MVP matrix (At least for the M part)
+		mvp*= glm::rotate(0.05f, glm::vec3(0.0f, 1.0f, 0.0f));
 		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &mvp[0][0]);
 		render();
 		// Swap buffers
